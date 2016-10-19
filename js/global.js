a06c8cb117fca60f6ee8f2ec913faf728953ccc8
Sally Wieland <sallybwieland@gmail.com>
2016-10-18 20:35:15 -0400

    second commit with comments created and some edits made

--- js/global.js
+++ js/global.js
@@ -1,3 +1,4 @@
+// all objects below contain the content that the createCardResults function will fill in
 var cardContent = {
   image: 'http://unsplash.it/400?random',
   title: 'Vintage Board Game',
@@ -73,28 +74,27 @@ var cardContent12 = {
 
 
 var rowResults = document.createElement('div')
-rowResults.classList.add('row')
-document.getElementById('searchResultContainer').appendChild(rowResults)
+rowResults.classList.add('row') // creating row to insert columns
+document.getElementById('searchResultContainer').appendChild(rowResults) // searchResultContainer is the container id in which this row is contained
 
 
-function createCardResults(content){
+function createCardResults(content){ // creating the function that grabs the object list from above and places content where specified
 
   var col = document.createElement('div')
   col.className = 'col-sm-3 card img-rounded'
   col.style.boxShadow = '1px solid black'
   col.style.marginBottom = '30px'
-  col.setAttribute('title','What a nice listing')
   rowResults.appendChild(col)
 
   var cardRow1 = document.createElement('div')
   cardRow1.classList.add('cardPicture')
   cardRow1.classList.add('img-responsive')
-  cardRow1.innerHTML = '<img src="'+content.image+'" />'
+  cardRow1.innerHTML = '<img src="'+content.image+'" />' // grabbing the image property.
   col.appendChild(cardRow1)
 
   var cardRow2 = document.createElement('div')
   cardRow2.classList.add('cardTitle')
-  cardRow2.innerHTML = content.title
+  cardRow2.innerHTML = content.title // grabbing the title property.
   col.appendChild(cardRow2)
 
   var cardRow3 = document.createElement('div')
@@ -106,7 +106,7 @@ function createCardResults(content){
   cardRow3Brand.classList.add('brandText')
   cardRow3Brand.classList.add('text-muted')
   cardRow3Brand.style.padding = '0'
-  cardRow3Brand.innerHTML = content.seller
+  cardRow3Brand.innerHTML = content.seller // grabbing the seller property.
   cardRow3.appendChild(cardRow3Brand)
 
   var cardRow3Price = document.createElement('div')
@@ -115,10 +115,11 @@ function createCardResults(content){
   cardRow3Price.classList.add('text-success')
   cardRow3Price.classList.add('text-right')
   cardRow3Price.style.padding = '0'
-  cardRow3Price.innerHTML = content.price
+  cardRow3Price.innerHTML = content.price // grabbing the price property.
   cardRow3.appendChild(cardRow3Price)
 }
 
+// calling the function.
 createCardResults(cardContent)
 createCardResults(cardContent2)
 createCardResults(cardContent3)
